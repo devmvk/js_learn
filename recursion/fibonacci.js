e38f0e53c5800c2b24f3f40647695be8b5f2148d
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-04-07 20:19:44 +0530

    fibonacci export

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -1,6 +1,7 @@
-function fibonacci(n) {
+const fibonacci = (x) => {
     if(n == 1 || n== 2) return 1;
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-console.log(fibonacci(7))
+export {fibonacci};
+
