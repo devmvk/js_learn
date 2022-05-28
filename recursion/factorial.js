dbb35f3cbb5b42fe318acf0a014794ad7307624d
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-05-28 23:18:46 +0530

    refactors factorial

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -1,6 +1,17 @@
-const factorial = (value) => {
+function factorial(value){
     if(value == 1) return 1;
-    return value * getFactorial(value-1)
-} 
+    return value * factorial(value-1)
+}
 
-export {factorial};
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+  readline.question(`Enter the number?`, n => {
+    let result = factorial(n)
+    console.log(result)
+    readline.close();
+  });
+  
